68eb297681425c71d545b0fa4cfb7b280c8372b8
jonathancsr <jonathancsrr@gmail.com>
2022-06-20 14:05:08 -0300

    refac: remove dist

--- mf-router6/packages/store/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type Movie = {
-    title: string;
-    image: string;
-};
-export declare const useStore: import("zustand").UseBoundStore<import("zustand").StoreApi<{
-    movies: Movie[];
-    addMovie: (movie: Movie) => void;
-}>>;
-//# sourceMappingURL=index.d.ts.map
--- mf-router6/packages/store/dist/index.js
+++
@@ -1,5 +0,0 @@
-import create from 'zustand';
-export const useStore = create((set) => ({
-    movies: [],
-    addMovie: (movie) => set((state) => ({ movies: [...state.movies, movie] }))
-}));
