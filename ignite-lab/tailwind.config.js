9874353b852235a4fd0188fa2108d688c81a9f1a
jonathancsr <jonathancsrr@gmail.com>
2022-06-22 14:21:17 -0300

    feat: add default font

--- ignite-lab/tailwind.config.js
+++ ignite-lab/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      },
       colors: {
         green: {
           300: "#00B37E",
